2eb3d836cc6315db2a4893feeaafdd517445355a
zackhsw <zackhsw318@outlook.com>
2023-06-01 22:40:32 +0800

    feat:kitties

--- pallets/kitties/src/lib.rs
+++ pallets/kitties/src/lib.rs
@@ -10,9 +10,14 @@ pub mod pallet {
 	use frame_support::pallet_prelude::*;
 	use frame_system::pallet_prelude::*;
 
+	use frame_support::traits::Randomness;
+	use sp_io::hashing::blake2_128;
+
 	pub type KittyId = u32;
-	#[derive(Encode, Decode, Clone, Copy, RuntimDebug, PartialEq,Eq,Default,TypeInfo,MaxEncodedLen)]
-	pub struct Kitty(pub [u8;16]);
+	#[derive(
+		Encode, Decode, Clone, Copy, RuntimeDebug, PartialEq, Eq, Default, TypeInfo, MaxEncodedLen,
+	)]
+	pub struct Kitty(pub [u8; 16]);
 
 	#[pallet::pallet]
 	pub struct Pallet<T>(_);
@@ -22,21 +27,25 @@ pub mod pallet {
 	pub trait Config: frame_system::Config {
 		/// Because this pallet emits events, it depends on the runtime's definition of an event.
 		type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
+		type Randomness: Randomness<Self::Hash, Self::BlockNumber>;
 	}
 
 	#[pallet::storage]
 	#[pallet::getter(fn next_kitty_id)]
 	pub type NextKittyId<T> = StorageValue<_, KittyId, ValueQuery>;
 
-
 	#[pallet::storage]
 	#[pallet::getter(fn kitties)]
-	pub type Kitties<T> = StorageMap<_, Blake2_128Concat, KittyId,Kitty>;
+	pub type Kitties<T> = StorageMap<_, Blake2_128Concat, KittyId, Kitty>;
 
 	#[pallet::storage]
 	#[pallet::getter(fn kitty_owner)]
-	pub type KittyOwner<T:Config> = StorageMap<_, Blake2_128Concat, KittyId, T::AccountId>;
+	pub type KittyOwner<T: Config> = StorageMap<_, Blake2_128Concat, KittyId, T::AccountId>;
 
+	#[pallet::storage]
+	#[pallet::getter(fn kitty_parents)]
+	pub type KittyParents<T: Config> =
+		StorageMap<_, Blake2_128Concat, KittyId, (KittyId, KittyId), OptionQuery>;
 
 	// Pallets use events to inform users when important changes are made.
 	// https://docs.substrate.io/main-docs/build/events-errors/
@@ -45,7 +54,21 @@ pub mod pallet {
 	pub enum Event<T: Config> {
 		/// Event documentation should end with an array that provides descriptive names for event
 		/// parameters. [something, who]
-		KittyCreated { who: T::AccountId, kitty_id:KittyId, kitty: Kitty},
+		KittyCreated {
+			who: T::AccountId,
+			kitty_id: KittyId,
+			kitty: Kitty,
+		},
+		KittyBred {
+			who: T::AccountId,
+			kitty_id: KittyId,
+			kitty: Kitty,
+		},
+		KittyTransferred {
+			who: T::AccountId,
+			recipient: T::AccountId,
+			kitty_id: KittyId,
+		},
 	}
 
 	// Errors inform users that something went wrong.
@@ -53,6 +76,8 @@ pub mod pallet {
 	pub enum Error<T> {
 		/// Error names should be descriptive.
 		InvalidKittyId,
+		SameKittyId,
+		NotOwner,
 	}
 
 	// Dispatchable functions allows users to interact with the pallet and invoke state changes.
@@ -71,11 +96,10 @@ pub mod pallet {
 			let who = ensure_signed(origin)?;
 
 			let kitty_id = Self::get_next_id()?;
-			let kitty = Kitty(Default::default());
+			let kitty = Kitty(Self::random_value(&who));
 
-			Kitties::<T>::insert(kitty_id, &kitty):
+			Kitties::<T>::insert(kitty_id, &kitty);
 			KittyOwner::<T>::insert(kitty_id, &who);
-
 
 			// Emit an event.
 			Self::deposit_event(Event::KittyCreated { who, kitty_id, kitty });
@@ -83,16 +107,76 @@ pub mod pallet {
 			Ok(())
 		}
 
-		
+		#[pallet::call_index(1)]
+		#[pallet::weight(10_000 + T::DbWeight::get().writes(1).ref_time())]
+		pub fn breed(
+			origin: OriginFor<T>,
+			kitty_id_1: KittyId,
+			kitty_id_2: KittyId,
+		) -> DispatchResult {
+			let who = ensure_signed(origin)?;
+			ensure!(kitty_id_1 != kitty_id_2, Error::<T>::SameKittyId);
+			ensure!(Kitties::<T>::contains_key(kitty_id_1), Error::<T>::InvalidKittyId);
+			ensure!(Kitties::<T>::contains_key(kitty_id_2), Error::<T>::InvalidKittyId);
+
+			let kitty_id = Self::get_next_id()?;
+			let kitty_1 = Self::kitties(kitty_id_1).ok_or(Error::<T>::InvalidKittyId)?;
+			let kitty_2 = Self::kitties(kitty_id_2).ok_or(Error::<T>::InvalidKittyId)?;
+
+			let selector = Self::random_value(&who);
+			let mut data = [0u8; 16];
+			for i in 0..kitty_1.0.len() {
+				data[i] = (kitty_1.0[i] & selector[i]) | (kitty_2.0[i] & !selector[i]);
+			}
+			let kitty = Kitty(data);
+
+			Kitties::<T>::insert(kitty_id, &kitty);
+			KittyOwner::<T>::insert(kitty_id, &who);
+			KittyParents::<T>::insert(kitty_id, (kitty_id_1, kitty_id_2));
+
+			// Emit an event.
+			Self::deposit_event(Event::KittyBred { who, kitty_id, kitty });
+			// Return a successful DispatchResultWithPostInfo
+			Ok(())
+		}
+
+		#[pallet::call_index(2)]
+		#[pallet::weight(10_000)]
+		pub fn transfer(
+			origin: OriginFor<T>,
+			recipient: T::AccountId,
+			kitty_id: KittyId,
+		) -> DispatchResult {
+			let who = ensure_signed(origin)?;
+			ensure!(KittyOwner::<T>::contains_key(kitty_id), Error::<T>::InvalidKittyId);
+
+			let owner = Self::kitty_owner(kitty_id).ok_or(Error::<T>::InvalidKittyId)?;
+			ensure!(owner == who, Error::<T>::NotOwner);
+
+			KittyOwner::<T>::insert(kitty_id, &recipient);
+			Self::deposit_event(Event::KittyTransferred { who, recipient, kitty_id });
+			Ok(())
+		}
 	}
-	
-	impl <T: Config> Pallet<T> {
-		fn get_next_id() -> Result<KittyId, DispatchError>{
-			NextKittyId::<T>::try_mutate(|next_id| -> Result<KittyId, DispatchError>{
-			let current_id = *next_id;
-			*next_id = next_id.checked_add(1).ok_or(Error::<T>::InvalidKittyId.info())?;
-			Ok(current_id)				
+
+	impl<T: Config> Pallet<T> {
+		fn get_next_id() -> Result<KittyId, DispatchError> {
+			NextKittyId::<T>::try_mutate(|next_id| -> Result<KittyId, DispatchError> {
+				let current_id = *next_id;
+				*next_id = next_id
+					.checked_add(1)
+					.ok_or::<DispatchError>(Error::<T>::InvalidKittyId.into())?;
+				Ok(current_id)
 			})
+		}
+
+		fn random_value(sender: &T::AccountId) -> [u8; 16] {
+			let payload = (
+				T::Randomness::random_seed(),
+				&sender,
+				<frame_system::Pallet<T>>::extrinsic_index(),
+			);
+			payload.using_encoded(blake2_128)
 		}
 	}
 }
---
+++ pallets/kitties/src/test.rs
@@ -0,0 +1,7 @@
+use crate::{Error, mock::*};
+use frame_support::{assert_ok, assert_noop};
+
+#[test]
+fn it_test(){
+    assert_eq!(1,1);
+}
